78538d693b8d7a87d610c6d6d64322ac17e2c778
chell <tisenac2019@gmail.com>
2021-03-02 21:32:37 -0300

    aula020321

--- src/app/pages/user-list/user-list.page.ts
+++ src/app/pages/user-list/user-list.page.ts
@@ -19,6 +19,8 @@ export class UserListPage implements OnInit {
   ngOnInit() {
     this.userService.getAll().subscribe(
       res=>{
+        this.users = res;
+        console.log(this.users);
 
       },
   
